724f4f6d41d2d596a6b74c1b25ca8c3d4171dcf5
FadedUnder0 <64360573+FadedUnder0@users.noreply.github.com>
2020-04-26 13:35:59 +0100

    Update example.lua

--- example.lua
+++ example.lua
@@ -2,7 +2,7 @@
 LOGIN:
 local username = ""
 local password = ""
-local lib = loadstring(game:HttpGet("https://pastebin.com/raw/XPawsNn4",true))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/FadedUnder0/uilib/master/lib.lua",true))()
 local main = lib:window("login")
 main:textbox("Username", function(text)
     username = text
@@ -15,21 +15,21 @@ main:button("login", function()
 end)
 
 BUTTON EXAMPLE:
-local lib = loadstring(game:HttpGet("https://pastebin.com/raw/XPawsNn4",true))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/FadedUnder0/uilib/master/lib.lua",true))()
 local main = lib:window("button")
 main:button("name", function()
     print("hi!")
 end)
 
 TEXTBOX EXAMPLE:
-local lib = loadstring(game:HttpGet("https://pastebin.com/raw/XPawsNn4",true))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/FadedUnder0/uilib/master/lib.lua",true))()
 local main = lib:window("button")
 main:textbox("name", function(text)
     print(text)
 end)
 
 CHECKBOX EXAMPLE:
-local lib = loadstring(game:HttpGet("https://pastebin.com/raw/XPawsNn4",true))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/FadedUnder0/uilib/master/lib.lua",true))()
 local main = lib:window("button")
 main:checkbox("name", function(chc)
   if(chc == true)
@@ -41,13 +41,11 @@ end
 end)
 
 LABEL:
-local lib = loadstring(game:HttpGet("https://pastebin.com/raw/XPawsNn4",true))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/FadedUnder0/uilib/master/lib.lua",true))()
 local main = lib:window("button")
 main:label("name")
 
 SLIDER:
-local lib = loadstring(game:HttpGet("https://pastebin.com/raw/XPawsNn4",true))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/FadedUnder0/uilib/master/lib.lua",true))()
 local slider = lib:slider("title", min,max,location,function(e) print(e) end)
 local slider = lib:slider("print value", 0,100,0,function(e) print(e) end) --will start at 0
-
-]]--
